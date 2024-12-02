11e3a6d7d6ece437bae7368b7a6661aaa9527846
utty-23479 <utty.23479@gmail.com>
2024-12-02 02:23:27 -0600

    vite.config.js update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "frontend/dist",
   },
-  base: "/task4userdashboard",
 });
